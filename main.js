01525373c3455441fa665d78302031cb9824147b
Lorcann <contact@lorcannrauzduel.fr>
2022-09-06 23:19:16 +0200

    add refreshPairs() function to test

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const PairService  = require('./services/PairService');
+const pairService = new PairService();
 
 const startArbitrage = async () => {
-  const pairService = new PairService();
   const pairs = await pairService.getAllPairs();
   pairService.identifyOpportunities(pairs);
 }
 
+const refreshPairs = async () => {
+  try {
+    await pairService.updatePairs();
+  } catch (error) {
+    console.log({error})
+  }
+}
+
+// refreshPairs();
 startArbitrage();
