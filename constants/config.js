7c75f9c830dfabb793abd5d4217e41fe5deef5df
Lorcann <contact@lorcannrauzduel.fr>
2022-09-06 23:19:31 +0200

    add sushi factory address

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,10 @@
 module.exports = {
     provider: "https://bsc-dataseed.binance.org",
-    
+
     factoryAddress: {
-        PANCAKE: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',
-        BISWAP: "0x858E3312ed3A876947EA49d572A7C42DE08af7EE"
+        PANCAKE: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
+        BISWAP: "0x858E3312ed3A876947EA49d572A7C42DE08af7EE",
+        SUSHI: "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac"
     },
 
     router: {
@@ -14,12 +15,12 @@ module.exports = {
     baseTokens: {
         usdt: { symbol: "USDT", address: "0x55d398326f99059ff775485246999027b3197955" },
         wbnb: { symbol: "WBNB", address: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c" },
-        busd: { symbol: 'BUSD', address: '0xe9e7cea3dedca5984780bafc599bd69add087d56' }
+        busd: { symbol: "BUSD", address: "0xe9e7cea3dedca5984780bafc599bd69add087d56" }
     },
 
     quoteTokens: {
-        eth: { symbol: 'ETH', address: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8' },
-        cake: { symbol: 'CAKE', address: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82' },
+        eth: { symbol: "ETH", address: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8" },
+        cake: { symbol: "CAKE", address: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82" },
     }
 }
     
